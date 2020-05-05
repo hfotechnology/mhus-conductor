085ace87b002fa9d13354b959ab0bdd2ed49552f
Mike Hummel <mike.hummel@billingsolutions.de>
2020-05-05 20:43:51 +0200

    prepare docker mojo

--- conductor-plugin/src/main/java/de/mhus/con/plugin/DockerMojo.java
+++ conductor-plugin/src/main/java/de/mhus/con/plugin/DockerMojo.java
@@ -17,17 +17,21 @@ public class DockerMojo extends MLog implements ExecutePlugin {
     public boolean execute(Context context) throws Exception {
         String dockerPath = ConUtil.cmdLocation(context.getConductor(), "docker");
         String namespace = context.getRecursiveProperty("dockerNamespace", null);
-        
+
         String cmd = dockerPath + " " + (namespace == null ? "" : "-ns " + namespace + " ") + MString.join(context.getStep().getArguments(), " ");
-        
+
         String path = context.getProject().getProperties().getString("dockerPath", null);
-        if (path == null)
+        if (path == null) {
+            log().d("dockerPath not found in project");
             return false;
-        
+        }
+
         File dir = new File(context.getProject().getRootDir(), path);
-        if (!dir.exists() || !dir.isDirectory())
+        if (!dir.exists() || !dir.isDirectory()) {
+            log().d("dockerPath not exists",path,dir.getAbsolutePath());
             return false;
-        
+        }
+
         String[] res = ConUtil.execute("docker " + context.getProject().getName() + "/" + path, dir, cmd, true);
         if (!res[2].equals("0"))
             throw new MojoException(context, "not successful",cmd,res[1],res[2]);
